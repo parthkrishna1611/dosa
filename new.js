e62b1d684137e7553335ea244a0cb8cee89376b4
parthkrishna1611 <parthkrishna1611@gmail.com>
2017-10-03 10:15:44 +0530

    added all the files as of now

---
+++ new.js
@@ -0,0 +1,23 @@
+// Function for top nav bar
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+// Function for manual image sliding
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
